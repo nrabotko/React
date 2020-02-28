800c59ba27fa5e4136fed4b03a93271ec18d053d
nrabotko <nrabotko99@gmail.com>
2020-02-28 13:14:12 +0300

    initialCommit

---
+++ App.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react'
+import { StyleSheet, Text, View, FlatList } from 'react-native'
+import { Navbar } from './src/components/Navbar'
+import { MainScreen } from './src/screens/MainScreen'
+import { TodoScreen } from './src/screens/TodoScreen'
+
+export default function App() {
+  const [todoId, setTodoId] = useState('2')
+  const [todos, setTodos] = useState([
+    { id: '1', title: 'Выучить React Native' },
+    { id: '2', title: 'Написать приложение' }
+  ])
+
+  const addTodo = title => {
+    setTodos(prev => [
+      ...prev,
+      {
+        id: Date.now().toString(),
+        title
+      }
+    ])
+  }
+
+  const removeTodo = id => {
+    setTodos(prev => prev.filter(todo => todo.id !== id))
+  }
+
+  let content = (
+    <MainScreen
+      todos={todos}
+      addTodo={addTodo}
+      removeTodo={removeTodo}
+      openTodo={setTodoId}
+    />
+  )
+
+  if (todoId) {
+    const selectedTodo = todos.find(todo => todo.id === todoId)
+    content = <TodoScreen goBack={() => setTodoId(null)} todo={selectedTodo} />
+  }
+
+  return (
+    <View>
+      <Navbar title='Todo App!' />
+      <View style={styles.container}>{content}</View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 30,
+    paddingVertical: 20
+  }
+})
---
+++ src/screens/TodoScreen.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { StyleSheet, View, Text, Button } from 'react-native'
+
+export const TodoScreen = ({ goBack, todo }) => {
+  return (
+    <View>
+      <Text>{todo.title}</Text>
+      <View style={styles.buttons}>
+        <View style={styles.button}>
+          <Button title='Назад' onPress={goBack} color='#757575' />
+        </View>
+        <View style={styles.button}>
+          <Button
+            title='Удалить'
+            color='#e53935'
+            onPress={() => console.log('To Remove')}
+          />
+        </View>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  button: {
+    width: '40%'
+  }
+})
