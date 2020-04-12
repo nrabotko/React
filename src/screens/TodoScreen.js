e4ad2a8d59d289f148b2870652aafd35f7c31d9c
nrabotko <nrabotko99@gmail.com>
2020-04-12 15:53:20 +0300

    Changing screen rotation

--- src/screens/TodoScreen.js
+++ src/screens/TodoScreen.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import { StyleSheet, View, Button } from 'react-native'
+import { StyleSheet, View, Button, Dimensions } from 'react-native'
 import { FontAwesome, AntDesign } from '@expo/vector-icons'
 import {THEME} from '../theme'
 import { AppCard } from '../components/ui/AppCard'
@@ -55,7 +55,7 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between'
   },
   button: {
-    width: '40%'
+    width: Dimensions.get('window').width / 3
   },
   card:{
     marginBottom: 20,
